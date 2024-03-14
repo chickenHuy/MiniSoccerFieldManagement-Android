ad6c3507764d229de741d3db3e5210016435a639
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-03-14 09:08:47 +0700

    Update interface login

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/activity/MainActivity.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/activity/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setStatusBarColor();
+    }
+
+    public void setStatusBarColor() {
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary, getTheme()));
     }
 }
