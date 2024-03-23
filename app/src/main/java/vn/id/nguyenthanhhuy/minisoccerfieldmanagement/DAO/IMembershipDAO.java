e888cbefdb3792a6e082ff25c82318213baf6c54
LuanNguyen <thienluan678@gmail.com>
2024-03-23 10:11:36 +0700

    Modify type of function in IMatchRecord, IBooking, AND Add new class Customer Membership, DAO

---
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/DAO/IMembershipDAO.java
@@ -0,0 +1,16 @@
+package vn.id.nguyenthanhhuy.minisoccerfieldmanagement.DAO;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.model.Membership;
+
+public interface IMembershipDAO {
+    Boolean add(Membership membership);
+    Boolean update(Membership membership);
+    Boolean softDelete(String id);
+    Membership findById(String id);
+    List<Membership> findAll();
+    Membership findBySpendAmount(BigDecimal totalSpend);
+    int findDiscountByCustomer(String customerId);
+}
