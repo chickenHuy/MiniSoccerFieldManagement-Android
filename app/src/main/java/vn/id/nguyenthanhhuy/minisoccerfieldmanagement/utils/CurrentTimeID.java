702d2eb97f46be5c9b7b16cc01c4e28c25168325
Khan9Tran <trankhangtctv@gmail.com>
2024-03-24 15:37:21 +0700

    thêm service price list, id tự động

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/utils/CurrentTimeID.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/utils/CurrentTimeID.java
@@ -1,4 +1,15 @@
 package vn.id.nguyenthanhhuy.minisoccerfieldmanagement.utils;
+import androidx.annotation.NonNull;
 
-public class TimeBaseIDGenerator {
+import java.util.concurrent.atomic.AtomicReference;
+public class CurrentTimeID {
+    private static AtomicReference<Long> currentTime =
+            new AtomicReference<>(System.currentTimeMillis());
+
+    @NonNull
+    public static String nextId(String prefix) {
+        return prefix + currentTime.accumulateAndGet(System.currentTimeMillis(),
+                (prev, next) -> next > prev ? next : prev + 1).toString();
+    }
+
 }
