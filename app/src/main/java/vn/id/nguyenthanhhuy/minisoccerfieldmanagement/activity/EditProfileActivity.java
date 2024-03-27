b54efb11b938637651608bf9bf7ca4dfdb4de45c
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-03-27 10:32:19 +0700

    Modify Edit profile activity

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/activity/EditProfileActivity.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/activity/EditProfileActivity.java
@@ -2,40 +2,72 @@ package vn.id.nguyenthanhhuy.minisoccerfieldmanagement.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.yariksoffice.lingver.Lingver;
 
+import java.util.Calendar;
+
 import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.R;
 import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.application.MainApplication;
+import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.databinding.ActivityEditProfileBinding;
+import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.model.User;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private ActivityEditProfileBinding binding;
 
-    private AutoCompleteTextView autoCompleteTextView;
+    private String option;
+    private User currentUer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        binding = ActivityEditProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Lingver.getInstance().setLocale(EditProfileActivity.this, MainApplication.language);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            option = extras.getString("option");
+            if (option.equals("editProfile")) {
+                currentUer = (User) extras.getSerializable("currentUser");
+            }
+
+            Toast.makeText(this, option, Toast.LENGTH_SHORT).show();
+        }
 
         setStatusBarColor();
-        configureWidget();
-        Lingver.getInstance().setLocale(EditProfileActivity.this, MainApplication.language);
+        setWidget();
     }
 
     public void setStatusBarColor() {
         getWindow().setStatusBarColor(getResources().getColor(R.color.primaryColor, getTheme()));
     }
 
-    public void configureWidget() {
+    public void setWidget() {
+        if (option.equals("editProfile")) {
 
+        }
+        binding.buttonShowCalender.setOnClickListener(v -> {
+            final Calendar calendar = Calendar.getInstance();
+            int mYear = calendar.get(Calendar.YEAR);
+            int mMonth = calendar.get(Calendar.MONTH);
+            int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(EditProfileActivity.this,
+                    (view, year, monthOfYear, dayOfMonth) -> {
+                        String date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        binding.editTextDateOfBirth.setText(date);
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        });
     }
 
     public void saveChangeProfile(View view) {
-        MainApplication.language = "vi";
-        Lingver.getInstance().setLocale(EditProfileActivity.this, MainApplication.language);
-        recreate();
+
     }
 }
--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/model/User.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/model/User.java
@@ -1,8 +1,9 @@
 package vn.id.nguyenthanhhuy.minisoccerfieldmanagement.model;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-public class User {
+public class User implements Serializable {
     private String id;
     private String name;
     private String gender;
