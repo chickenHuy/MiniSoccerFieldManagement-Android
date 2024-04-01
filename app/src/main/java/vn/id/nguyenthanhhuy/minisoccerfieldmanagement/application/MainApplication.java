bddbdcf36a6af8e04af2b9d95858f15ee5c26f76
LuanNguyen <thienluan678@gmail.com>
2024-04-01 23:14:37 +0700

    Tắt login

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/application/MainApplication.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/application/MainApplication.java
@@ -5,15 +5,37 @@ import android.app.Application;
 import com.yariksoffice.lingver.Lingver;
 
 import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.model.User;
+import vn.id.nguyenthanhhuy.minisoccerfieldmanagement.utils.CurrentTimeID;
 
 public class MainApplication extends Application {
     public static String language = "en";
     public static User curentUser = null;
+
+    User newUser = new User();
+
+
     public static boolean isChangeLanguage = false;
     @Override
     public void onCreate() {
         super.onCreate();
         Lingver.init(MainApplication.this, language);
         Lingver.getInstance().setLocale(MainApplication.this, language);
+
+        createfakeUser();
+    }
+
+    private void createfakeUser() {
+        newUser.setName("Nguyễn Thiện Luân");
+        newUser.setUserName("1");
+        newUser.setPassword("admin");
+        newUser.setId(CurrentTimeID.nextId("user"));
+        newUser.setDateOfBirth("08/12/2003");
+        newUser.setImage(null);
+        newUser.setPhoneNumber("0889192145");
+        newUser.setRole("Manager");
+        newUser.setDeleted(false);
+        newUser.setType(null);
+        newUser.setGender("Nam");
+        curentUser = newUser;
     }
 }
