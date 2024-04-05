9279366bf6207431fbfa3e0147de5e865fb1f9c8
LuanNguyen <thienluan678@gmail.com>
2024-04-06 01:21:34 +0700

    Complete Membership

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/fragment/ShowUserProfileFragment.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/fragment/ShowUserProfileFragment.java
@@ -202,7 +202,7 @@ public class ShowUserProfileFragment extends Fragment {
     public void onResume() {
         super.onResume();
         // Update user information when the fragment resumes
-        setWidget();
+        //setWidget();
         setInformation();
     }
 
@@ -273,6 +273,7 @@ public class ShowUserProfileFragment extends Fragment {
                     onResume();
                 } else {
                     Toast.makeText(getContext(), "Update Image failed", Toast.LENGTH_SHORT).show();
+                    currentUser.setImage(null);
                 }
             }
         }
