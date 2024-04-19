c44741e7678889f5e2ae5e8790d25badcce1de1a
Tran Lam Nhut Khang <92084108+Khan9Tran@users.noreply.github.com>
2024-04-19 20:27:28 +0700

    sơ sơ thống kê

--- app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/utils/StaticString.java
+++ app/src/main/java/vn/id/nguyenthanhhuy/minisoccerfieldmanagement/utils/StaticString.java
@@ -9,5 +9,8 @@ public class StaticString {
     public static final String COMPLETED = "completed";
     public static final String STAFF = "staff";
     public static final String MANAGER = "manager";
+    public static final String FINAL_AMOUNT = "finalAmount";
+    public static final String FIELD_AMOUNT = "field";
+    public static final String SERVICE_AMOUNT = "service";
 
 }
